0841ab5a75a884dd5c32f30dcacdd52290a6299f
Muhammad Taufik <muhammad.taufik@adira.co.id>
2026-02-28 06:52:01 +0700

    add class room route api

--- api/v1/class-room/index.js
+++ api/v1/class-room/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const controller = require('./controller');
 
 router.get('/list/:page', controller.getClassRoom);
+router.get('/list/:page/:search', controller.getClassRoom);
 router.post('/create', controller.createClassRoom);
 
 module.exports = router;
