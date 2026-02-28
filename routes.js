b1a50f171b7933345effaf564236a7cfe57e5736
Muhammad Taufik <muhammad.taufik@adira.co.id>
2026-02-28 09:17:16 +0700

    add teacher route api

--- routes.js
+++ routes.js
@@ -7,7 +7,10 @@ const logger = require('./config/logger');
 /* SET CORS HEADERS FOR API */
 router.all('/api/*', (req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
     next();
 })
 
