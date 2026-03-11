1403253cc50228fc693bd7a1e00cde844fb992b3
Muhammad Taufik <muhammad.taufik@adira.co.id>
2026-03-12 06:39:59 +0700

    uopdate code

--- api/v1/jurnal/controller.js
+++ api/v1/jurnal/controller.js
@@ -11,14 +11,16 @@ const ApiErrorMsg = require('../../../error/apiErrorMsg');
 const HttpStatusCode = require("../../../error/httpStatusCode");
 const adrJurnalMengajar = require('../../../model/adr_jurnal_mengajar');
 const adrSiswa = require('../../../model/adr_siswa');
-const adrJurnalMengajarDetailSiswa = require('../../../model/adr_jurnal_mengajar_detail_siswa');
 const adrClassRoom = require('../../../model/adr_class_room');
 const adrTeacher = require('../../../model/adr_teacher');
 const adrClassLevelSilabus = require('../../../model/adr_class_level_silabus');
+const adrJurnalMengajarDetailSiswa = require('../../../model/adr_jurnal_mengajar_detail_siswa');
+const adrJurnalMengajarDetailSilabus = require('../../../model/adr_jurnal_mengajar_detail_silabus');
 
 exports.createJurnalMengajar = async function (req, res) {
   const transaction = await sequelize.transaction();
   try {
+    let pushSilabus = [];
     const id_jurnal = uuidv7();
     const tanggal = req.body.tanggal;
     const mulai = req.body.mulai;
@@ -35,9 +37,11 @@ exports.createJurnalMengajar = async function (req, res) {
         is_deleted: 0
       }
     })
-    if (!dataKelas) {
+    if (!dataKelas || !dataKelas?.id_tingkat_kelas) {
       throw new ApiErrorMsg(HttpStatusCode.BAD_REQUEST, '70010');
     }
+
+    const idTingkatKelas = dataKelas?.id_tingkat_kelas;
 
     const dataGuru = await adrTeacher.findOne({
       raw: true,
@@ -71,6 +75,50 @@ exports.createJurnalMengajar = async function (req, res) {
       throw new ApiErrorMsg(HttpStatusCode.BAD_REQUEST, '70010');
     }
 
+    const levelSilabus = await adrClassLevelSilabus.findAll({
+      raw: true,
+      where: {
+        id_tingkat_kelas: idTingkatKelas,
+        is_deleted: 0,
+      }
+    })
+    
+    if (levelSilabus.length == 0) {
+      throw new ApiErrorMsg(HttpStatusCode.BAD_REQUEST, '70010');
+    }
+
+    if (levelSilabus.length > 0) {
+      for (let i = 0; i < levelSilabus.length; i++) {
+        const silabus = await sequelize.query(`SELECT adr_silabus.id, adr_silabus.nama, 
+        adr_silabus_items.id as item_id, adr_silabus_items.nama as nama_item
+        FROM adr_silabus LEFT JOIN adr_silabus_items
+        ON adr_silabus.id = adr_silabus_items.kode_silabus
+        where adr_silabus.id = :id_ AND adr_silabus.is_deleted = '0'`,
+          { replacements: { id_: `${levelSilabus[i].id_silabus}` }, type: sequelize.QueryTypes.SELECT },
+          {
+            raw: true
+          });
+
+        if (silabus.length) {
+          for (let j = 0; j < silabus.length; j++) {
+            const result = {
+              id: uuidv7(),
+              created_dt: moment().format('YYYY-MM-DD HH:mm:ss.SSS'),
+              created_by: req.id,
+              is_deleted: 0,
+              id_jurnal: id_jurnal,
+              id_silabus: levelSilabus[i].id_silabus,
+              id_item_silabus: silabus[j].item_id,
+              item_silabus: silabus[j].nama_item,
+              penilaian: null
+            };
+
+            pushSilabus.push(result);
+          }
+        }
+      }
+    }
+
     await adrJurnalMengajar.create({
       id: id_jurnal,
       created_dt: moment().format('YYYY-MM-DD HH:mm:ss.SSS'),
@@ -90,6 +138,10 @@ exports.createJurnalMengajar = async function (req, res) {
     }, {transaction})
 
     await adrJurnalMengajarDetailSiswa.bulkCreate(result, {
+      transaction
+    });
+
+    await adrJurnalMengajarDetailSilabus.bulkCreate(pushSilabus, {
       transaction
     });
 
@@ -129,54 +181,10 @@ exports.getDetailJurnalMengajar = async function (req, res) {
         id_jurnal: data?.id
       }
     })
-
-    const dataKelasDanTingkatKelas = await adrClassRoom.findOne({
-      raw: true,
-      where: {
-        id: id_kelas
-      }
-    })
-    const idTingkatKelas = dataKelasDanTingkatKelas?.id_tingkat_kelas;
-    const levelSilabus = await adrClassLevelSilabus.findAll({
-      raw: true,
-      where: {
-        id_tingkat_kelas: idTingkatKelas,
-        is_deleted: 0,
-      }
-    })
     
-    if (levelSilabus.length > 0) {
-      for (let i = 0; i < levelSilabus.length; i++) {
-        const silabus = await sequelize.query(`SELECT adr_silabus.id, adr_silabus.nama, 
-        adr_silabus_items.id as item_id, adr_silabus_items.nama as nama_item
-        FROM adr_silabus LEFT JOIN adr_silabus_items
-        ON adr_silabus.id = adr_silabus_items.kode_silabus
-        where adr_silabus.id = :id_ AND adr_silabus.is_deleted = '0'`,
-          { replacements: { id_: `${levelSilabus[i].id_silabus}` }, type: sequelize.QueryTypes.SELECT },
-          {
-            raw: true
-          });
-
-        if (silabus.length) {
-          const result = {
-            id: silabus[0]?.id,
-            title: silabus[0]?.nama,
-            items: silabus
-              .filter(item => item.nama_item !== null)
-              .map(item => ({
-                id: item?.item_id,
-                nama_item: item?.nama_item
-              }))
-          };
-          pushSilabus.push(result)
-        }
-      }
-    }
-
     const hasil = {
       jurnal: data,
-      siswa: detail,
-      silabus: pushSilabus
+      siswa: detail
     }
 
     return res.status(200).json(rsMsg('000000', hasil))
---
+++ model/adr_jurnal_mengajar_detail_silabus.js
@@ -0,0 +1,24 @@
+const Sequelize = require('sequelize');
+const dbConnection = require('../config/db').Sequelize;
+
+const adrJurnalMengajarDetailSilabus = dbConnection.define('adr_jurnal_mengajar_detail_silabus', {
+  id: {
+    type: Sequelize.STRING,
+    primaryKey: true
+  },
+  created_dt: Sequelize.DATE(6),
+  created_by: Sequelize.STRING,
+  modified_dt: Sequelize.DATE(6),
+  modified_by: Sequelize.STRING,
+  is_deleted: Sequelize.INTEGER,
+  id_jurnal: Sequelize.STRING,
+  id_silabus: Sequelize.STRING,
+  item_silabus: Sequelize.STRING,
+  penilaian: Sequelize.STRING,
+}, {
+  freezeTableName: true,
+  timestamps: false,
+  tableName: 'adr_jurnal_mengajar_detail_silabus'
+});
+
+module.exports = adrJurnalMengajarDetailSilabus;
