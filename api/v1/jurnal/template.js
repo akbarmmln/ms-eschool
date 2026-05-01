3410113b68901a38e693cd77e69f273d94b659d4
Muhammad Taufik <muhammad.taufik@adira.co.id>
2026-05-01 10:08:46 +0700

    update code

--- api/v1/jurnal/template.js
+++ api/v1/jurnal/template.js
@@ -105,88 +105,124 @@ exports.htmlSinglePenilaianHarian = async function (data) {
 
             <body>
                 ${data.map((siswa) => `
-                <div class='page'>
-                    <div class='header'>
-                    <div class='header-left'>
-                        <p><b>Nama</b> : ${siswa.nama_siswa}</p>
-                        <p><b>Tanggal</b> : ${hariTanggalIndo(siswa.tanggal)}</p>
-                    </div>
-                    <div class='header-right'>
-                        <p><b>Materi</b> : ${siswa.materi}</p>
-                        <p><b>Refleksi</b> : ${siswa.refleksi}</p>
-                    </div>
-                    </div>
-
-                    <table>
-                        <thead>
-                            <tr>
-                                <th rowspan='2'>No</th>
-                                <th rowspan='2'>Aktifitas</th>
-                                <th colspan='4'>Hasil</th>
-                                <th rowspan='2'>Keterangan</th>
-                            </tr>
-                            <tr>
-                                <th>BSB</th>
-                                <th>BSH</th>
-                                <th>MB</th>
-                                <th>BB</th>
-                            </tr>
-                        </thead>
-
-                        <tbody>
-                            ${siswa.items.map((item, i) => `
-                            <tr>
-                                <td>${i + 1}</td>
-                                <td class='text-left'>${item.item_silabus}</td>
-                                
-                                <td class='check'>${item.nilai == "1" ? `<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='currentColor' class='icon icon-tabler icons-tabler-filled icon-tabler-check'><path stroke='none' d='M0 0h24v24H0z' fill='none'/><path d='M20.707 6.293a1 1 0 0 1 0 1.414l-10 10a1 1 0 0 1 -1.414 0l-5 -5a1 1 0 0 1 1.414 -1.414l4.293 4.293l9.293 -9.293a1 1 0 0 1 1.414 0' /></svg>` : ''}</td>
-                                <td class='check'>${item.nilai == "2" ? `<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='currentColor' class='icon icon-tabler icons-tabler-filled icon-tabler-check'><path stroke='none' d='M0 0h24v24H0z' fill='none'/><path d='M20.707 6.293a1 1 0 0 1 0 1.414l-10 10a1 1 0 0 1 -1.414 0l-5 -5a1 1 0 0 1 1.414 -1.414l4.293 4.293l9.293 -9.293a1 1 0 0 1 1.414 0' /></svg>` : ''}</td>
-                                <td class='check'>${item.nilai == "3" ? `<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='currentColor' class='icon icon-tabler icons-tabler-filled icon-tabler-check'><path stroke='none' d='M0 0h24v24H0z' fill='none'/><path d='M20.707 6.293a1 1 0 0 1 0 1.414l-10 10a1 1 0 0 1 -1.414 0l-5 -5a1 1 0 0 1 1.414 -1.414l4.293 4.293l9.293 -9.293a1 1 0 0 1 1.414 0' /></svg>` : ''}</td>
-                                <td class='check'>${item.nilai == "4" ? `<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='currentColor' class='icon icon-tabler icons-tabler-filled icon-tabler-check'><path stroke='none' d='M0 0h24v24H0z' fill='none'/><path d='M20.707 6.293a1 1 0 0 1 0 1.414l-10 10a1 1 0 0 1 -1.414 0l-5 -5a1 1 0 0 1 1.414 -1.414l4.293 4.293l9.293 -9.293a1 1 0 0 1 1.414 0' /></svg>` : ''}</td>
-
-                                <td class=text-left>${item.keterangan || ''}</td>
-                            </tr>
-                            `).join("")}
-                        </tbody>
-                    </table>
-
-                    <!-- KETERANGAN -->
-                    <div class='footer small'>
-                        <p>
-                            Ket :
-                            BSB = Berkembang Sangat Baik &nbsp;&nbsp;
-                            BSH = Berkembang Sesuai Harapan &nbsp;&nbsp;
-                            MB = Mulai Berkembang &nbsp;&nbsp;
-                            BB = Belum Berkembang
-                        </p>
-                    </div>
-
-                    <div class='ttd-wrapper'>
-                        <!-- BARIS ATAS -->
-                        <div class='ttd-row'>
-                            <div class='ttd-col center'>
-                                <p>Class Teacher</p>
-                                <br><br><br><br>
-                                <b class="underline-text">${siswa.nama_guru ?? ''}</b><br>
-                                <b>NIY : ${siswa.niy_guru ?? '-'}</b>
-                            </div>
-
-                            <div class='ttd-col center'>
-                                <p>Parents,</p>
-                                <br><br><br><br>
-                                -------------------------
-                            </div>
+                    <div class='page'>
+                        <div class='header'>
+                        <div class='header-left'>
+                            <p><b>Nama</b> : ${siswa.nama_siswa}</p>
+                            <p><b>Tanggal</b> : ${hariTanggalIndo(siswa.tanggal)}</p>
+                        </div>
+                        <div class='header-right'>
+                            <p><b>Materi</b> : ${siswa.materi}</p>
+                            <p><b>Refleksi</b> : ${siswa.refleksi}</p>
+                        </div>
                         </div>
 
-                        <!-- BARIS BAWAH -->
-                        <div class='ttd-bottom center'>
-                            <p>Kinder Principal</p>
-                            <br><br><br><br>
-                            <b class="underline-text">${siswa.nama_principal}</b><br>
-                            <b>NIY : ${siswa.niy_principal}</b>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th rowspan='2'>No</th>
+                                    <th rowspan='2'>Aktifitas</th>
+                                    <th colspan='4'>Hasil</th>
+                                    <th rowspan='2'>Keterangan</th>
+                                </tr>
+                                <tr>
+                                    <th>BSB</th>
+                                    <th>BSH</th>
+                                    <th>MB</th>
+                                    <th>BB</th>
+                                </tr>
+                            </thead>
+
+                            <tbody>
+                                ${siswa.items.map((item, i) => `
+                                <tr>
+                                    <td>${i + 1}</td>
+                                    <td class='text-left'>${item.item_silabus}</td>
+                                    
+                                    <td class='check'>${item.nilai == "1" ? `<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='currentColor' class='icon icon-tabler icons-tabler-filled icon-tabler-check'><path stroke='none' d='M0 0h24v24H0z' fill='none'/><path d='M20.707 6.293a1 1 0 0 1 0 1.414l-10 10a1 1 0 0 1 -1.414 0l-5 -5a1 1 0 0 1 1.414 -1.414l4.293 4.293l9.293 -9.293a1 1 0 0 1 1.414 0' /></svg>` : ''}</td>
+                                    <td class='check'>${item.nilai == "2" ? `<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='currentColor' class='icon icon-tabler icons-tabler-filled icon-tabler-check'><path stroke='none' d='M0 0h24v24H0z' fill='none'/><path d='M20.707 6.293a1 1 0 0 1 0 1.414l-10 10a1 1 0 0 1 -1.414 0l-5 -5a1 1 0 0 1 1.414 -1.414l4.293 4.293l9.293 -9.293a1 1 0 0 1 1.414 0' /></svg>` : ''}</td>
+                                    <td class='check'>${item.nilai == "3" ? `<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='currentColor' class='icon icon-tabler icons-tabler-filled icon-tabler-check'><path stroke='none' d='M0 0h24v24H0z' fill='none'/><path d='M20.707 6.293a1 1 0 0 1 0 1.414l-10 10a1 1 0 0 1 -1.414 0l-5 -5a1 1 0 0 1 1.414 -1.414l4.293 4.293l9.293 -9.293a1 1 0 0 1 1.414 0' /></svg>` : ''}</td>
+                                    <td class='check'>${item.nilai == "4" ? `<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='currentColor' class='icon icon-tabler icons-tabler-filled icon-tabler-check'><path stroke='none' d='M0 0h24v24H0z' fill='none'/><path d='M20.707 6.293a1 1 0 0 1 0 1.414l-10 10a1 1 0 0 1 -1.414 0l-5 -5a1 1 0 0 1 1.414 -1.414l4.293 4.293l9.293 -9.293a1 1 0 0 1 1.414 0' /></svg>` : ''}</td>
+
+                                    <td class=text-left>${item.keterangan || ''}</td>
+                                </tr>
+                                `).join("")}
+                            </tbody>
+                        </table>
+
+                        <!-- KETERANGAN -->
+                        <div class='footer small'>
+                            <p>
+                                Ket :
+                                BSB = Berkembang Sangat Baik &nbsp;&nbsp;
+                                BSH = Berkembang Sesuai Harapan &nbsp;&nbsp;
+                                MB = Mulai Berkembang &nbsp;&nbsp;
+                                BB = Belum Berkembang
+                            </p>
+                        </div>
+
+                        <div class='ttd-wrapper'>
+                            <!-- BARIS ATAS -->
+                            <div class='ttd-row'>
+                                <div class='ttd-col center'>
+                                    <p>Class Teacher</p>
+                                    <br><br><br><br>
+                                    <b class="underline-text">${siswa.nama_guru ?? ''}</b><br>
+                                    <b>NIY : ${siswa.niy_guru ?? '-'}</b>
+                                </div>
+
+                                <div class='ttd-col center'>
+                                    <p>Parents,</p>
+                                    <br><br><br><br>
+                                    -------------------------
+                                </div>
+                            </div> 
+
+                            <!-- BARIS BAWAH -->
+                            <div class='ttd-bottom center'>
+                                <p>Kinder Principal</p>
+                                <br><br><br><br>
+                                <b class="underline-text">${siswa.nama_principal}</b><br>
+                                <b>NIY : ${siswa.niy_principal}</b>
+                            </div>
                         </div>
                     </div>
-                </div>
+                    <div class='page'>
+                        <div class="grid">
+                            <div class="card">
+                                <div class="card-header">
+                                <small>COMPUTATIONAL THINKING</small>
+                                <h3>algoritma</h3>
+                                </div>
+
+                                <div class="section">
+                                <label>Achievement Level</label>
+                                <div class="achievement">
+                                    <div class="badge">BB</div>
+                                    <div class="badge">MB</div>
+                                    <div class="badge active blue">BSH</div>
+                                    <div class="badge">BSB</div>
+                                </div>
+                                </div>
+
+                                <div class="section">
+                                <label>Upload Foto Tugas (Attachments)</label>
+                                <div class="upload-row">
+                                    <div class="preview">
+                                    <img src="https://via.placeholder.com/100">
+                                    </div>
+
+                                    <div class="upload-box">+</div>
+                                </div>
+                                </div>
+
+                                <div class="section">
+                                <label>Keterangan (Notes)</label>
+                                <textarea placeholder="Tambah catatan perkembangan khusus..."></textarea>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 `).join("")}
             </body>
         </html>
