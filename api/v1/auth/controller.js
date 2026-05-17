17381ed693c04c92996b8efb0550289a3bf65c58
Muhammad Taufik <muhammad.taufik@adira.co.id>
2026-05-17 07:40:54 +0700

    update code

--- api/v1/auth/controller.js
+++ api/v1/auth/controller.js
@@ -554,7 +554,102 @@ exports.userMenus = async function (req, res) {
         name: 'Role Akses',
         icon: 'menu-icon icon-base ri ri-shield-user-line',
         href: '/akademik/role-akses',
-      }
+      },
+      {
+        name: 'Role Akses',
+        icon: 'menu-icon icon-base ri ri-shield-user-line',
+        href: '/akademik/role-akses',
+      },
+      {
+        name: 'Role Akses',
+        icon: 'menu-icon icon-base ri ri-shield-user-line',
+        href: '/akademik/role-akses',
+      },
+      {
+        name: 'Role Akses',
+        icon: 'menu-icon icon-base ri ri-shield-user-line',
+        href: '/akademik/role-akses',
+      },
+      {
+        name: 'Role Akses',
+        icon: 'menu-icon icon-base ri ri-shield-user-line',
+        href: '/akademik/role-akses',
+      },
+      {
+        name: 'Role Akses',
+        icon: 'menu-icon icon-base ri ri-shield-user-line',
+        href: '/akademik/role-akses',
+      },
+      {
+        name: 'Role Akses',
+        icon: 'menu-icon icon-base ri ri-shield-user-line',
+        href: '/akademik/role-akses',
+      },
+      {
+        name: 'Role Akses',
+        icon: 'menu-icon icon-base ri ri-shield-user-line',
+        href: '/akademik/role-akses',
+      },
+      {
+        name: 'Role Akses',
+        icon: 'menu-icon icon-base ri ri-shield-user-line',
+        href: '/akademik/role-akses',
+      },
+      {
+        name: 'Role Akses',
+        icon: 'menu-icon icon-base ri ri-shield-user-line',
+        href: '/akademik/role-akses',
+      },
+      {
+        name: 'Role Akses',
+        icon: 'menu-icon icon-base ri ri-shield-user-line',
+        href: '/akademik/role-akses',
+      },
+      {
+        name: 'Role Akses',
+        icon: 'menu-icon icon-base ri ri-shield-user-line',
+        href: '/akademik/role-akses',
+      },
+      {
+        name: 'Role Akses',
+        icon: 'menu-icon icon-base ri ri-shield-user-line',
+        href: '/akademik/role-akses',
+      },
+      {
+        name: 'Role Akses',
+        icon: 'menu-icon icon-base ri ri-shield-user-line',
+        href: '/akademik/role-akses',
+      },
+      {
+        name: 'Role Akses',
+        icon: 'menu-icon icon-base ri ri-shield-user-line',
+        href: '/akademik/role-akses',
+      },
+      {
+        name: 'Role Akses',
+        icon: 'menu-icon icon-base ri ri-shield-user-line',
+        href: '/akademik/role-akses',
+      },
+      {
+        name: 'Role Akses',
+        icon: 'menu-icon icon-base ri ri-shield-user-line',
+        href: '/akademik/role-akses',
+      },
+      {
+        name: 'Role Akses',
+        icon: 'menu-icon icon-base ri ri-shield-user-line',
+        href: '/akademik/role-akses',
+      },
+      {
+        name: 'Role Akses',
+        icon: 'menu-icon icon-base ri ri-shield-user-line',
+        href: '/akademik/role-akses',
+      },
+      {
+        name: 'Role Akses',
+        icon: 'menu-icon icon-base ri ri-shield-user-line',
+        href: '/akademik/role-akses',
+      },
     ]
     return res.status(200).json(rsMsg('000000', menus))
   } catch (e) {
